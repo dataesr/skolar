72eb0935d84d0a6e2826f1f41a664c4d7f748b95
Victor BARBIER <vicozer@gmail.com>
2025-06-24 13:18:28 +0200

    feat(inference): get instruction from model

--- project/server/main/inference/predict.py
+++ project/server/main/inference/predict.py
@@ -1,7 +1,28 @@
 import requests
+from huggingface_hub import hf_hub_download
 
 
-def predict(url: str, messages: list):
+def get_instruction_from_hub(repo_id: str) -> str:
+    # Download file
+    file_path = hf_hub_download(repo_id, filename="instruction.txt", repo_type="model")
+
+    # Read file
+    with open(file_path, "r", encoding="utf-8") as file:
+        instruction = file.read()
+
+    return instruction
+
+
+instruction = get_instruction_from_hub("dataesr/openchat-3.6-8b-acknowledgments")  # TODO: model from env ?
+
+
+def chatml_messages(texts: list[str]) -> list:
+    # Format texts to chatml
+    messages = [[{"role": "system", "content": instruction}, {"role": "user", "content": text}] for text in texts]
+    return messages
+
+
+def predict(url: str, texts: list[str]):
     """
     Request on inference app to generate prediction
 
@@ -12,8 +33,13 @@ def predict(url: str, messages: list):
     Returns:
         predictions (list[str]): list of predictions
     """
+    # Format to chatml with instruction from model
+    messages = chatml_messages(texts)
+
+    # Request model api
     body = {"messages": messages, "use_chatml": True}
     response = requests.post(f"{url}/predict", data=body)
     response.raise_for_status()
     json = response.json()
+
     return json.get("prediction")
