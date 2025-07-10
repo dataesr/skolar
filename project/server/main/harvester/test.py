77bc51a843637d790cbbde7d6b0bc0c3faa8c353
eric <eric.jeangirard@recherche.gouv.fr>
2025-07-10 12:53:34 +0200

    fix

--- project/server/main/harvester/test.py
+++ project/server/main/harvester/test.py
@@ -6,7 +6,7 @@ from project.server.main.harvester.elsevier_client import ElsevierClient
 from project.server.main.harvester.config import config
 from project.server.main.harvester.download_publication_utils import publisher_api_download, standard_download, SUCCESS_DOWNLOAD, safe_instanciation_client, FAIL_DOWNLOAD
 from project.server.main.grobid import run_grobid
-from project.server.main.utils import get_filename
+from project.server.main.utils import get_filename, get_elt_id
 from project.server.main.logger import get_logger
 logger = get_logger(__name__)
 
@@ -23,17 +23,12 @@ except:
 def process_entry(elt, worker_idx = 1):
     global wiley_client, elsevier_client
     result = FAIL_DOWNLOAD
-    elt_id = elt.get('id')
-    doi = elt.get('doi')
-    if doi and isinstance(doi, str):
-        assert(doi.startswith('10.'))
-        doi = doi.lower()
-    if doi and not isinstance(elt_id, str):
-        elt_id = f'doi{doi}'
-        elt['id'] = elt_id
+    elt_id = get_elt_id(elt)
+    elt['id'] = elt_id
     filename = get_filename(elt_id, f'pdf_{worker_idx}')
-    if os.path.isfile(filename):
-        logger.debug(f'already downloaded {filename} for {elt_id}')
+    filename_xml = get_filename(elt_id, 'grobid')
+    if os.path.isfile(filename_xml):
+        logger.debug(f'already downloaded / grobidified {filename} for {elt_id}')
         return
     publisher = None
     if isinstance(elt.get('publisher_normalized'), str):
@@ -41,6 +36,7 @@ def process_entry(elt, worker_idx = 1):
     elif isinstance(elt.get('publisher'), str):
         publisher = elt['publisher']
     urls_to_test = []
+    doi = elt.get('doi')
     if doi and isinstance(doi, str):
         if wiley_client and 'wiley' in publisher.lower():
             result, _ = publisher_api_download(doi, filename, wiley_client)
@@ -66,6 +62,10 @@ def process_entry(elt, worker_idx = 1):
             url = oa_loc['url']
         logger.debug(url)
         if isinstance(url, str):
+            for g in ['medihal-', 'media.hal']:
+                if g in url:
+                    logger.debug('medihal is skipped')
+                    return
             result = FAIL_DOWNLOAD
             try:
                 result, _ = standard_download(url, filename, elt_id)
